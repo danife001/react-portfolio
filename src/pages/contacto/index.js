b86b22beca59d428366b3e44e564528b09f6814a
Daniel <danife001@gmail.com>
2024-10-30 10:51:10 -0500

    cambio a Contacto index

--- src/pages/contacto/index.js
+++ src/pages/contacto/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Boton from '../../components/boton'
 import styles from './contacto.module.css'
 
@@ -22,29 +21,4 @@ function Contacto(){
     </div>)
 }
 
-=======
-import Boton from '../../components/boton'
-import styles from './contacto.module.css'
-
-function Contacto(){
-
-    return(<div className={styles.contenedor}>
-        <div className={styles.contacto}>
-            <div className={styles.titulo}>
-                <h1>Trabajemos</h1>
-                <h3>dime tu idea</h3>
-            </div>
-            <div>
-                <form  className={styles.formulario}>
-                    <input placeholder="Escribe aquí..."></input>
-                    <input placeholder="Escribe aquí..."></input>
-                    <textarea></textarea>
-                    <Boton texto="contactame"/>
-                </form>
-            </div>
-        </div>
-    </div>)
-}
-
->>>>>>> c0c07ef9e2ca9188decae3f5b5407059fbd2b533
 export default Contacto
